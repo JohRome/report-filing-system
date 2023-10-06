e5e8701903e765dd869bc345ca148c0e1378c305
Johan <jofiluren@outlook.com>
2023-10-06 07:00:50 +0200

    Initial commit

---
+++ src/test/java/johan/report/ReportFilingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package johan.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportFilingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
