8a9cb8b4f0019bebd13b4209b6c3b24bfabcf561
Johan <jofiluren@outlook.com>
2023-10-07 13:32:56 +0200

    added lombok to pojos

--- shared-pojos/src/main/java/com/johan/pojos/Address.java
+++ shared-pojos/src/main/java/com/johan/pojos/Address.java
@@ -1,50 +1,17 @@
 package com.johan.pojos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     private String street;
     private String apartmentNumber;
     private String city;
     private String zipCode;
-
-    public Address() {
-    }
-
-    public Address(String street, String apartmentNumber, String city, String zipCode) {
-        this.street = street;
-        this.apartmentNumber = apartmentNumber;
-        this.city = city;
-        this.zipCode = zipCode;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getApartmentNumber() {
-        return apartmentNumber;
-    }
-
-    public void setApartmentNumber(String apartmentNumber) {
-        this.apartmentNumber = apartmentNumber;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
 }
--- shared-pojos/src/main/java/com/johan/pojos/Person.java
+++ shared-pojos/src/main/java/com/johan/pojos/Person.java
@@ -1,40 +1,16 @@
 package com.johan.pojos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     private String firstName;
     private String lastName;
     private Address address;
-
-    public Person() {
-    }
-
-    public Person(String firstName, String lastName, Address address) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
 }
