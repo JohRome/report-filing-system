d05e77ef7f556f9cf3269cf77c12f6bfb8e27222
Johan <jofiluren@outlook.com>
2023-10-07 12:00:49 +0200

    to hell with test. Fixing mongo api now

--- server-side/src/main/java/com/johan/server/kafka/mongo/controller/MongoController.java
+++ server-side/src/main/java/com/johan/server/kafka/mongo/controller/MongoController.java
@@ -1,4 +1,32 @@
 package com.johan.server.kafka.mongo.controller;
 
+import com.johan.server.kafka.mongo.consumer.ReportRepository;
+import com.johan.server.kafka.mongo.entities.ReportEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/reports")
 public class MongoController {
+
+    private final ReportRepository reportRepository;
+
+    public MongoController(ReportRepository reportRepository) {
+        this.reportRepository = reportRepository;
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<List<ReportEntity>> getReports() {
+        try {
+            List<ReportEntity> reports = reportRepository.findAll();
+            return ResponseEntity.ok(reports);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
