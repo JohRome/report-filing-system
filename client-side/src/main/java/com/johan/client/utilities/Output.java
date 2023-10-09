aa74121a9e33dd3a13db53306b4b01213e593a16
Johan <jofiluren@outlook.com>
2023-10-09 08:01:22 +0200

    fixed better menus

--- client-side/src/main/java/com/johan/client/utilities/Output.java
+++ client-side/src/main/java/com/johan/client/utilities/Output.java
@@ -8,37 +8,40 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Output {
 
-    /**
-     * Displays the main menu options to the console.
-     */
     public static void printMainMenu() {
         log.info("""
-                 ----------------------------------------- MENU OPTIONS -------------------------------------------------
-                1 - Enter Kafka as a User
-                2 - Enter MongoDB as an Admin
-                3 - Exit program
-                ----------------------------------------- MAKE A CHOICE ------------------------------------------------
+                       MAIN MENU OPTIONS
+                 ----------------------------
+                | 1 - Enter Kafka as User    |
+                | 2 - Enter MongoDB as Admin |
+                | 3 - Exit program           |
+                 ----------------------------
+                       MAKE A MENU CHOICE
                 """);
     }
 
     public static void printKafkaMenu() {
         log.info("""
-                ----------------------------------------- MENU OPTIONS -------------------------------------------------
-                1 - File a Disturbance Report
-                2 - Print all messages in a Kafka topic
-                3 - Back to main menu
-                ----------------------------------------- MAKE A CHOICE ------------------------------------------------
+                       KAFKA MENU OPTIONS
+                 -------------------------------
+                | 1 - File a Disturbance Report |
+                | 2 - Get messages in topic     |
+                | 3 - Back to main menu         |
+                 -------------------------------
+                       MAKE A MENU CHOICE
                 """);
     }
 
     public static void printMongoAdminMenu() {
         log.info("""
-                ----------------------------------------- MENU OPTIONS -------------------------------------------------
-                1 - Print all reports
-                2 - Patch a report by id
-                3 - Delete a report by id
-                4 - Back to main menu
-                ----------------------------------------- MAKE A CHOICE ------------------------------------------------
+                  MONGO ADMIN MENU OPTIONS
+                 ---------------------------
+                | 1 - Print all reports     |
+                | 2 - Patch a report by id  |
+                | 3 - Delete a report by id |
+                | 4 - Back to main menu     |
+                 ---------------------------
+                     MAKE A MENU CHOICE
                 """);
     }
 
